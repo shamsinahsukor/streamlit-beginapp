18d12f93bc3edb2b667414e6caa9aa250d0485df
Shamsinah <111561557+shamsinahsukor@users.noreply.github.com>
2022-08-24 15:10:38 +0800

    Update beginapp1.py

--- beginapp1.py
+++ beginapp1.py
@@ -60,3 +60,16 @@ from PIL import Image
 image = Image.open('diabetes-type.jpg')
 
 st.image(image, caption='Diabetes Type')
+
+    'Starting a long computation...'
+    
+    latest_iteration = st.empty()
+    bar = st.progress(0)
+
+    for i in range(100):
+   
+        latest_iteration.text(f'Iteration {i+1}')
+        bar.progress(i + 1)
+        time.sleep(0.1)
+
+    '...and now we\'re done!'

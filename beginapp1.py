cf5680250cc200ed07be1c0a0a95d814bc13826e
Shamsinah <111561557+shamsinahsukor@users.noreply.github.com>
2022-08-24 15:00:37 +0800

    Update beginapp1.py

--- beginapp1.py
+++ beginapp1.py
@@ -1,3 +1,12 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+
+st.header("My first Streamlit App")
+st.write(pd.DataFrame({
+    'Intplan': ['yes', 'yes', 'yes', 'no'],
+    'Churn Status': [0, 0, 0, 1]
+}))
 
 import streamlit as st
 import pandas as pd

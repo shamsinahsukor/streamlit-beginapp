b20758db9a35916fae8d8abdb517d9c09e797428
Shamsinah <111561557+shamsinahsukor@users.noreply.github.com>
2022-08-24 14:51:14 +0800

    Update beginapp1.py

--- beginapp1.py
+++ beginapp1.py
@@ -16,6 +16,14 @@ image = Image.open('img-what-is-diabetes.jpg')
 
 st.image(image, caption='What is diabetes Type')
 
+Age Range,Count of Age
+20's,240
+30's,78
+40's,46
+50's,25
+60's,4
+80's,1
+
 
 st.header("Diabetes Types")
 from PIL import Image
